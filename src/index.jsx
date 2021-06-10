aef081d036e0ded3461c55d07370de738ea82b39
PLOGA <pfbtomas@gmail.com>
2021-06-10 17:32:30 +0100

    init

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import 'bootswatch/dist/sandstone/bootstrap.min.css'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
+
+reportWebVitals()
