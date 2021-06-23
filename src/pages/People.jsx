351831b9ed0371d3cb1fabfc3fd84695018d41ec
PLOGA <plugapt@gmail.com>
2021-06-23 17:59:56 +0100

    People now works

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { fetchPersons } from "../services/Caller"
 
 const People = () => {
+
+    const [persons, setPersons] = useState([])
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setPersons(await fetchPersons())
+        }
+        fetchAPI()
+    }, [])
+
+    const trendingPersons = persons.slice(0, 4).map((p, i) => {
+        return (
+            <div className="col-md-3 text-center" key={i}>
+                <img
+                    className="img-fluid rounded-circle mx-auto d-block"
+                    src={p.profileImg}
+                    alt={p.name}
+                ></img>
+                <p className="font-weight-bold text-center">{p.name}</p>
+                <p
+                    className="font-weight-light text-center"
+                    style={{ color: "#5a606b" }}
+                >
+                    Trending for {p.known}
+                </p>
+            </div>
+        )
+    })
+
   return (
-    <>
-      .
-    </>
+    <div className="container">
+        <div className="row mt-3">
+            <div className="col">
+                <p className="font-weight-bold" style={{ color: "#5a606b" }}>
+                    TRENDING PERSONS ON THIS WEEK
+                </p>
+            </div>
+        </div>
+
+        <div className="row mt-3">
+            <div className="col">
+                <div className="float-right">
+                    <i className="far fa-arrow-alt-circle-right"/>
+                </div>
+            </div>
+        </div>
+        <div className="row mt-3">{trendingPersons}</div>
+    </div>
+
+
   )
 }
 
--- src/services/Caller.jsx
+++ src/services/Caller.jsx
@@ -44,8 +44,22 @@ export const fetchMovieByGenre = async () => {
 }
 
 
-export const fetchPersons = () => {
-
+export const fetchPersons = async () => {
+  try {
+    const { data } = await axios.get(personUrl, {
+      params: {
+        api_key: apiKey
+      }
+    })
+    const modifiedData = data['results'].map((p) => ({
+      id: p['id'],
+      popularity: p['popularity'],
+      name: p['name'],
+      profileImg: 'https://image.tmdb.org/t/p/w200' + p['profile_path'],
+      known: p['known_for_department']
+    }))
+    return modifiedData;
+  } catch (error) { }
 }
 
 export const fetchTopRatedMovies = async () => {
